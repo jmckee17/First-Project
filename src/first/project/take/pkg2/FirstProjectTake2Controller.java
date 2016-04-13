a46ff3b58d9fdb3457630b0d581366f0c051796a
John McKee <jmckee@ucls.uchicago.edu>
2016-04-13 13:45:11 -0500

    end of day 413

--- src/first/project/take/pkg2/FirstProjectTake2Controller.java
+++ src/first/project/take/pkg2/FirstProjectTake2Controller.java
@@ -3,45 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package first.project.take.pkg2;
+package datagrapher;
 
+import com.google.gson.Gson;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import java.util.Scanner;
+import java.util.TreeMap;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
 
 /**
  *
- * @author csstudent
+ * @author dwheadon
  */
-public class FirstProjectTake2Controller implements Initializable {
+public class MainController implements Initializable {
+        
+    @FXML
+    private StackedBarChart<String,Number> chart;
     
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello");
-    }
+    private LineChart<String,Number> percentageChart;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        String s = "http://apps.who.int/gho/athena/data/GHO/WHS4_544.json?profile=simple&filter=YEAR:1980";
-        URL url = null;
+        String s = "https://data.cityofchicago.org/resource/4ijn-s7e5.json?$select=zip,results";
+        URL myUrl = null;
         try {
-            url = new URL(s);
+            myUrl = new URL(s);
         } catch (Exception e) {
             System.out.println("Improper URL " + s);
-            System.exit(-1);
         }
      
         // read from the URL
         Scanner scan = null;
         try {
-            scan = new Scanner(url.openStream());
+            scan = new Scanner(myUrl.openStream());
         } catch (Exception e) {
             System.out.println("Could not connect to " + s);
             System.exit(-1);
@@ -53,10 +56,61 @@ public class FirstProjectTake2Controller implements Initializable {
         }
         scan.close();
 
+        Map<Integer, Integer> failedInspections = new TreeMap<Integer, Integer>();
+        Map<Integer, Integer> unfailedInspections = new TreeMap<Integer, Integer>();
         Gson gson = new Gson();
-        Fact fact = gson.fromJson(str, Fact.class);
+        Inspection[] inspections = gson.fromJson(str, Inspection[].class);
 
-        System.out.println(fact);
+        // Add all the zips to both maps
+        for (Inspection inspection : inspections) {
+            Integer zip = inspection.getZip();
+            if (! failedInspections.containsKey(zip)) {
+                failedInspections.put(zip, 0);
+                unfailedInspections.put(zip, 0);
+            }
+        }
+        for (Inspection inspection : inspections) {
+            Integer zip = inspection.getZip();
+            if (inspection.failed()) {
+                Integer currFails = failedInspections.get(zip);
+                failedInspections.put(zip, currFails + 1);
+            } else {
+                Integer curTotal = unfailedInspections.get(zip);
+                unfailedInspections.put(zip, curTotal + 1);
+            }
+        }
+
+        XYChart.Series<String, Number> failedSeries = new XYChart.Series();
+        failedSeries.setName("# Failed Inspections");
+        Object[] keys = failedInspections.keySet().toArray();
+        Arrays.sort(keys);
+        for (Object zip : keys) {
+            failedSeries.getData().add(new XYChart.Data(zip.toString(), failedInspections.get(zip)));
+        }
+        chart.getData().add(failedSeries);
+
+        XYChart.Series<String, Number> totalSeries = new XYChart.Series<>();
+        XYChart.Series<String, Number> percentageSeries = new XYChart.Series();
+        totalSeries.setName("# Unfailed Inspections");
+        keys = unfailedInspections.keySet().toArray();
+        Arrays.sort(keys);
+        for (Object zip : keys) {
+            int unfailed = unfailedInspections.get(zip);
+            int failed = 0;
+            if (failedInspections.containsKey(zip)) {
+                failed = failedInspections.get(zip);
+            }
+            int total = unfailed + failed;
+            totalSeries.getData().add(new XYChart.Data(zip.toString(), unfailed));
+            percentageSeries.getData().add(new XYChart.Data(zip.toString(), (double) failed / total));
+        }
+        chart.getData().add(totalSeries);
+        percentageChart.getData().add(percentageSeries);
+        percentageChart.getXAxis().setTickLabelsVisible(false);
+        percentageChart.getYAxis().setTickLabelsVisible(false);        
+        
+        //double x1 = chart.getYAxis().computeAreaInScreen();
+        //percentageChart.setLayoutX(chart.getYAxis().getWidth());
     }    
     
 }
